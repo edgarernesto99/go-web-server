4c2791b2838017a21baa8ad15c6594bb35c2c000
Edgar Guzman <edgar.guzman6009@alumnos.udg.mx>
2020-11-29 18:56:31 -0600

    Implementacion de rutas y funciones para obtener promedio de alumno y de una materia

--- server.go
+++ server.go
@@ -82,6 +82,84 @@ func setGrade(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func formGetStudentAverage(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	fmt.Fprintf(
+		res,
+		cargarHtml("formGetStudentAverage.html"),
+		studentsToSelect(),
+	)
+}
+
+func showStudentAverage(res http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(res, "ParseForm() error %v", err)
+		return
+	}
+	student := req.FormValue("student")
+	var average float64
+	err := getStudentAverage(student, &average)
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	if err != nil {
+		fmt.Fprintf(
+			res,
+			cargarHtml("error.html"),
+			err,
+		)
+	} else {
+		fmt.Fprintf(
+			res,
+			cargarHtml("showAverage.html"),
+			"Promedio de "+student+": "+fmt.Sprint(average),
+		)
+	}
+}
+
+func formGetSubjectAverage(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	fmt.Fprintf(
+		res,
+		cargarHtml("formGetSubjectAverage.html"),
+		subjectsToSelect(),
+	)
+}
+
+func showSubjectAverage(res http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(res, "ParseForm() error %v", err)
+		return
+	}
+	subject := req.FormValue("subject")
+	var average float64
+	err := getSubjectAverage(subject, &average)
+	res.Header().Set(
+		"Content-Type",
+		"text/html",
+	)
+	if err != nil {
+		fmt.Fprintf(
+			res,
+			cargarHtml("error.html"),
+			err,
+		)
+	} else {
+		fmt.Fprintf(
+			res,
+			cargarHtml("showAverage.html"),
+			"Promedio en "+subject+": "+fmt.Sprint(average),
+		)
+	}
+}
+
 //Funciones auxiliares
 func saveGrade(args Args) error {
 	_, subjectExists := subjects[args.Subject]
@@ -104,6 +182,38 @@ func saveGrade(args Args) error {
 	} else {
 		students[args.Student][args.Subject] = args.Grade
 	}
+	return nil
+}
+
+func getStudentAverage(student string, reply *float64) error {
+	_, studentExists := students[student]
+	if !studentExists {
+		return errors.New("El alumno no existe")
+	}
+	var average float64 = 0
+	var total float64 = 0
+	for _, grade := range students[student] {
+		average = average + grade
+		total = total + 1
+	}
+	average = average / total
+	*reply = average
+	return nil
+}
+
+func getSubjectAverage(subject string, reply *float64) error {
+	_, subjectExists := subjects[subject]
+	if !subjectExists {
+		return errors.New("La materia no existe")
+	}
+	var average float64 = 0
+	var total float64 = 0
+	for _, grade := range subjects[subject] {
+		average = average + grade
+		total = total + 1
+	}
+	average = average / total
+	*reply = average
 	return nil
 }
 
@@ -146,10 +256,30 @@ func subjectsToString() string {
 	return html
 }
 
+func studentsToSelect() string {
+	var html string
+	for student := range students {
+		html += "<option value='" + student + "' id='student'>" + student + "</option>"
+	}
+	return html
+}
+
+func subjectsToSelect() string {
+	var html string
+	for subject := range subjects {
+		html += "<option value='" + subject + "' id='subject'>" + subject + "</option>"
+	}
+	return html
+}
+
 func main() {
 	http.HandleFunc("/root", index) //Pagina de inicio
 	http.HandleFunc("/form-set-grade", formSetGrade) //Formulario agregar calificacion
 	http.HandleFunc("/set-grade", setGrade) //Guarda la calificacion y muestra si se completo
+	http.HandleFunc("/form-student-average", formGetStudentAverage) // Formulario obtener prom de estudiante
+	http.HandleFunc("/form-subject-average", formGetSubjectAverage) // Formulario obtener prom de materia
+	http.HandleFunc("/student-average", showStudentAverage) //Muestra promedio de estudiante
+	http.HandleFunc("/subject-average", showSubjectAverage) //Mostrar promedio de materia
 	fmt.Println("Corriendo servirdor...")
 	http.ListenAndServe(":9000", nil)
 }
